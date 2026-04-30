cb5b088f7ecf7bce0a87e82597620790ec78733c
24230 <24230eg@hvhs.school.nz>
2026-04-30 21:43:38 +0000

    Apr 30, 2026, 9:43 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,31 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Maths:
+****************************/
+console.log("Running t05_introduction.js");
+
+// Variables:
+let userName = "Ethan Gould";
+let currentYear = 2026;
+let userAge = 14;
+let userMoney = 13.30;
+let userBirthYear;
+let ageInTenYears;
+userBirthYear = currentYear - userAge;
+ageInTenYears = userAge + 10;
+
+/****************************
+Main code:
+****************************/
+console.log("Hi "+ userName +".");
+console.log("As of "+ currentYear +" you are "+ userAge +" years old.");
+console.log("You were born in "+ userBirthYear +".");
+console.log("You have "+ userMoney +" dollars.");
+console.log("In 10 years you will be "+ ageInTenYears +" years old.")
+userMoney = userMoney / 2;
+console.log("You spend half of your money, now you have "+ userMoney +" dollars.")
+userMoney = userMoney + 3;
+console.log("Then you get $3, now you have "+ userMoney +" dollars.")
+
+/****************************
+Functions
+****************************/
