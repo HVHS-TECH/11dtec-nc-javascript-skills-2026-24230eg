d81e12b4d10628f134bde0b09c7af0e4761fd892
24230 <24230eg@hvhs.school.nz>
2026-05-07 21:43:24 +0000

    May 7, 2026, 9:43 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,43 @@
-/* Paste your code from the last task */
+/****************************
+conditionals:
+****************************/
+console.log("Running t12_conditionals.js");
+
+// Variables:
+let num1 = "50";
+let num2 = 50;
+let userName;
+let currentYear = 2026;
+let userAge;
+let userMoney;
+let userBirthYear;
+let ageInTenYears;
+userBirthYear = currentYear - userAge;
+ageInTenYears = userAge + 10;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameFeild");
+const AGE_FIELD = document.getElementById("ageFeild");
+const MONEY_FIELD = document.getElementById("moneyFeild");
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions
+****************************/
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop.</p>";
+};
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>"+ _name +": $"+ _price +"</p>";
+};
+function getFormInput(){
+    userName = NAME_FIELD.value;
+    userAge = Math.floor(Number(AGE_FIELD.value));
+    userMoney = Number(MONEY_FIELD.value);
+    OUTPUT.innerHTML = "<p>Your name is "+ userName +".</p>";
+    if (!isNaN(userAge)){
+        OUTPUT.innerHTML += "<p>You are "+ userAge +" years old.</p>";
+    };
+    OUTPUT.innerHTML += "<p>You have "+ userMoney +"$.</p>";
+};
