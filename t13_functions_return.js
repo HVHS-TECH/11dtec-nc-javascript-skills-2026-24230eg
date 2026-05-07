223e76c1cc85b32260e4dd13eca640a1135a05a0
24230 <24230eg@hvhs.school.nz>
2026-05-07 21:48:44 +0000

    May 7, 2026, 9:48 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,46 @@
-/* Paste your code from the last task */
+/****************************
+functions return:
+****************************/
+console.log("Running t13_functions_return.js");
+
+// Variables:
+let userName;
+let currentYear = 2026;
+let userAge;
+let userMoney;
+let userBirthYear;
+let ageInTenYears;
+userBirthYear = currentYear - userAge;
+ageInTenYears = userAge + 10;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameFeild");
+const AGE_FIELD = document.getElementById("ageFeild");
+const MONEY_FIELD = document.getElementById("moneyFeild");
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions
+****************************/
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop.</p>";
+};
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>"+ _name +": $"+ _price +"</p>";
+};
+function getFormInput(){
+    userName = NAME_FIELD.value;
+    userAge = Math.floor(Number(AGE_FIELD.value));
+    userMoney = Number(MONEY_FIELD.value);
+    OUTPUT.innerHTML = "<p>Your name is "+ userName +".</p>";
+    if (!isNaN(userAge)){
+        OUTPUT.innerHTML += "<p>You are "+ userAge +" years old.</p>";
+    };
+    OUTPUT.innerHTML += "<p>You have "+ userMoney +"$.</p>";
+    if (userMoney >= 4){
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $4. You CAN afford a chocolate bar.</p>";
+    }else{
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $4. Sorry you CAN'T afford a chocolate bar.</p>";
+    }
+};
