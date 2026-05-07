825836b4bd4ea50ce71f40755d60e291ce5d3f82
24230 <24230eg@hvhs.school.nz>
2026-05-07 21:05:47 +0000

    May 7, 2026, 9:05 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,52 @@
-/* Paste your code from the last task */
+/****************************
+data types:
+****************************/
+console.log("Running t11_data_types.js");
+
+// Variables:
+let userName = "Ethan Gould";
+let currentYear = 2026;
+let userAge = 14;
+let userMoney = 13.30;
+let userBirthYear;
+let ageInTenYears;
+userBirthYear = currentYear - userAge;
+ageInTenYears = userAge + 10;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameFeild");
+
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions
+****************************/
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop.</p>";
+};
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>"+ _name +": $"+ _price +"</p>";
+};
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameFeild");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p>Your name is "+ userName +".</p>";
+}
+/*function start(){
+    userMoney += 13.30;
+    OUTPUT.innerHTML += "<h2>Added by Javascript.</h2>";
+    OUTPUT.innerHTML += "<p>Hi "+ userName +".</p>";
+    OUTPUT.innerHTML += "<p>As of "+ currentYear +" you are "+ userAge +" years old.</p>";
+    OUTPUT.innerHTML += "<p>You were born in "+ userBirthYear +".</p>";
+    OUTPUT.innerHTML += "<p>You have "+ userMoney +" dollars.</p>";
+    OUTPUT.innerHTML += "<p>In 10 years you will be "+ ageInTenYears +" years old.</p>";
+    userMoney = userMoney / 2;
+    OUTPUT.innerHTML += "<p>You spend half of your money, now you have "+ userMoney +" dollars.</p>";
+    userMoney = userMoney + 3;
+    OUTPUT.innerHTML += "<p>Then you get $3, now you have "+ userMoney +" dollars.</p>";
+    welcome();
+    displayProduct("Chocolate bar", 4);
+    displayProduct("Chips", 3);
+    displayProduct("Drink", 2.5);
+};*/
