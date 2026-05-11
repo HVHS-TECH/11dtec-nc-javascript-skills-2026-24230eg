b3b27e1ac9a8926b31c510e577dfba2b54ba201e
24230 <24230eg@hvhs.school.nz>
2026-05-11 23:35:09 +0000

    May 11, 2026, 11:35 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,61 @@
-/* Paste your code from the last task */
+/****************************
+arrays 1:
+****************************/
+console.log("Running t14_arrays_1.js");
+
+// Variables:
+let userName;
+let currentYear = 2026;
+let userAge;
+let userMoney;
+let userBirthYear;
+let ageInTenYears;
+let change;
+let userPrefrenceChocolate;
+let chocolateMessages = ["You loath chocolate.","Chocolate is meh.","Chocolate is pretty good.","Chocolate is the best thing EVER!!!!"];
+userBirthYear = currentYear - userAge;
+ageInTenYears = userAge + 10;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameFeild");
+const AGE_FIELD = document.getElementById("ageFeild");
+const MONEY_FIELD = document.getElementById("moneyFeild");
+const CHOCOLATE_SLIDER = document.getElementById("chocolateSlider");
+/****************************
+Main code:
+****************************/
+
+/****************************
+Functions
+****************************/
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop.</p>";
+};
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>"+ _name +": $"+ _price +"</p>";
+};
+function getFormInput(){
+    userName = NAME_FIELD.value;
+    userAge = Math.floor(Number(AGE_FIELD.value));
+    userMoney = (Math.floor((MONEY_FIELD.value)*100))/100;
+    userPrefrenceChocolate = (CHOCOLATE_SLIDER.value-1);
+    OUTPUT.innerHTML = "<p>Your name is "+ userName +".</p>";
+    if (!isNaN(userAge)){
+        OUTPUT.innerHTML += "<p>You are "+ userAge +" years old.</p>";
+    };
+    if (!isNaN(userMoney)){
+        OUTPUT.innerHTML += "<p>You have "+ userMoney +"$.</p>";
+        if (userMoney >= 4){
+            OUTPUT.innerHTML += "<p>A chocolate bar costs $4. You CAN afford a chocolate bar.</p>";
+        }
+        else{
+            OUTPUT.innerHTML += "<p>A chocolate bar costs $4. Sorry you CAN'T afford a chocolate bar.</p>";
+        };
+        calculateChange(userMoney, 4);
+    };
+    OUTPUT.innerHTML += "<p>"+ chocolateMessages[userPrefrenceChocolate]+"<p>";
+};
+function calculateChange(_money, _price){
+    if (_money >= _price){
+        OUTPUT.innerHTML += "<p>You will get "+ (Math.round((_money-_price)*100))/100 +"$ change.</p>";
+    };
+};
