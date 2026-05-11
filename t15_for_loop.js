dffbf51a94aed86844adda725e1568e456f6d245
24230 <24230eg@hvhs.school.nz>
2026-05-11 23:39:55 +0000

    May 11, 2026, 11:39 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -13,6 +13,7 @@ let ageInTenYears;
 let change;
 let userPrefrenceChocolate;
 let chocolateMessages = ["You loath chocolate.","Chocolate is meh.","Chocolate is pretty good.","Chocolate is the best thing EVER!!!!"];
+let userMilkNum;
 userBirthYear = currentYear - userAge;
 ageInTenYears = userAge + 10;
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
@@ -20,6 +21,7 @@ const NAME_FIELD = document.getElementById("nameFeild");
 const AGE_FIELD = document.getElementById("ageFeild");
 const MONEY_FIELD = document.getElementById("moneyFeild");
 const CHOCOLATE_SLIDER = document.getElementById("chocolateSlider");
+const MILK_RANGE = document.getElementById("milkRange");
 /****************************
 Main code:
 ****************************/
@@ -38,6 +40,7 @@ function getFormInput(){
     userAge = Math.floor(Number(AGE_FIELD.value));
     userMoney = (Math.floor((MONEY_FIELD.value)*100))/100;
     userPrefrenceChocolate = (CHOCOLATE_SLIDER.value-1);
+    userMilkNum = MILK_RANGE.value;
     OUTPUT.innerHTML = "<p>Your name is "+ userName +".</p>";
     if (!isNaN(userAge)){
         OUTPUT.innerHTML += "<p>You are "+ userAge +" years old.</p>";
@@ -53,6 +56,7 @@ function getFormInput(){
         calculateChange(userMoney, 4);
     };
     OUTPUT.innerHTML += "<p>"+ chocolateMessages[userPrefrenceChocolate]+"<p>";
+    for (userMilkNum)
 };
 function calculateChange(_money, _price){
     if (_money >= _price){
